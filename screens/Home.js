c4bc7d0614d715541d6d56412254926441410d63
brunobta1 <bruno.tomari.dam@alumnescostafreda.cat>
2023-05-11 13:40:32 +0200

    home
    home

--- screens/Home.js
+++ screens/Home.js
@@ -50,7 +50,6 @@ const images = {
 export default function Home({ navigation }) {
   const MAX_DISTANCE = 50;
 
-
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [reptes, setReptes] = useState([]);
@@ -102,19 +101,33 @@ export default function Home({ navigation }) {
   }, [navigation]);
 
   function distance(lat1, lon1, lat2, lon2) {
+    // Radi de la Terra en metres
     const R = 6371e3;
+
+    // Convertir latituds a radians
     const phi1 = (lat1 * Math.PI) / 180;
     const phi2 = (lat2 * Math.PI) / 180;
+
+    //Diferencia de latituds en radians
     const deltaPhi = ((lat2 - lat1) * Math.PI) / 180;
+
+    //Diferencia de longituds en radians
     const deltaLambda = ((lon2 - lon1) * Math.PI) / 180;
+
+    //Part angular de la formula del Haversine
     const a =
       Math.sin(deltaPhi / 2) * Math.sin(deltaPhi / 2) +
       Math.cos(phi1) *
         Math.cos(phi2) *
         Math.sin(deltaLambda / 2) *
         Math.sin(deltaLambda / 2);
+
+    //Calcular angle central
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    //Calcular distancia en metres
     const d = R * c;
+
     return d;
   }
 
